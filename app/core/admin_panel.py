faa934fd5d41dfd783b219b315fa4d552d1ffe25
K_Nasonkin <sticheg2580@gmail.com>
2023-05-12 00:11:17 +0300

    Added sqladmin

---
+++ app/core/admin_panel.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from sqladmin import ModelView, Admin
+from sqlalchemy.ext.asyncio import AsyncEngine
+
+from app.models.users import User
+
+
+class UserAdmin(ModelView, model=User):
+    column_list = [User.id, User.email]
+
+
+def setup_admin(app: FastAPI, engine: AsyncEngine):
+    admin = Admin(app, engine)
+    admin.add_view(UserAdmin)
--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
+from app.core.admin_panel import setup_admin
 from app.core.config import settings
+from app.db.session import engine
 
 app = FastAPI(
     title="My Simple Car", openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
+setup_admin(app, engine)
