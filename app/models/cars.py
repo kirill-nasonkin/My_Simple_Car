2f08e1c590f904abdb20a3d3f81aec218a991e94
Kirill_N <sticheg2580@gmail.com>
2023-07-04 19:58:50 +0300

    модели машины, двигателя, корпуса, ТО

---
+++ app/models/cars.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+from typing import List, Optional
+
+from sqlalchemy import Float, ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.db.base_class import Base
+
+
+class Body(Base):
+    title: Mapped[str] = mapped_column(String(255), unique=True)
+    default_body_avatar: Mapped[str] = mapped_column(String(255), unique=True)
+
+    cars: Mapped[List["Car"]] = relationship("Car", back_populates="body")
+
+
+class Engine(Base):
+    model: Mapped[str] = mapped_column(String(255), unique=True)
+    fuel_type: Mapped[str] = mapped_column(String(255))
+    volume: Mapped[float] = mapped_column(Float(1))
+    power: Mapped[Optional[int]] = mapped_column(default=volume * 68)
+
+    cars: Mapped[List["Car"]] = relationship("Car", back_populates="engine")
+
+
+class Maintenance(Base):
+    title: Mapped[str] = mapped_column(String(255))
+    date: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+    current_car_mileage: Mapped[int]
+    maintenance_interval: Mapped[int]
+    car_id: Mapped[int] = mapped_column(ForeignKey("car.id"))
+
+    car: Mapped["Car"] = relationship("Car", back_populates="maintenances")
+
+
+class Car(Base):
+    avatar: Mapped[str] = mapped_column(String(255))
+    vin: Mapped[str] = mapped_column(String(17), unique=True)
+    year_built: Mapped[int]
+    brand: Mapped[str] = mapped_column(String(255))
+    model: Mapped[str] = mapped_column(String(255))
+    mileage: Mapped[int]
+    avg_month_mil: Mapped[int] = mapped_column(default=400)
+    gearbox: Mapped[str] = mapped_column(String(255))  # мкпп \ акпп \ ...
+    body_id: Mapped[int] = mapped_column(ForeignKey("body.id"))
+    engine_id: Mapped[int] = mapped_column(ForeignKey("engine.id"))
+
+    body: Mapped["Body"] = relationship(back_populates="cars")
+    engine: Mapped["Engine"] = relationship(back_populates="cars")
+    maintenances: Mapped[List["Maintenance"]] = relationship(
+        back_populates="car"
+    )
