2e89efc4cf9e3f5dcd8a5b0c52d18b9e27bb76b6
Kirill_N <sticheg2580@gmail.com>
2023-07-05 17:56:08 +0300

    imports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,3 @@
-from .cars import Body, Car, Engine, Maintenance
+from .cars import Body, Car, Engine, Maintenance, Image
 from .documents import Document, DriverLicense, Insurance
 from .users import User
