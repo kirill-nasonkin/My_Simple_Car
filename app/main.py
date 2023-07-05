6731832df78b0efb9bd7b597d7fa89040c608e1a
Kirill_N <sticheg2580@gmail.com>
2023-07-05 15:12:44 +0300

    sqlmodel

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 
 from app.auth.config import auth_backend, fastapi_users
+from app.core.admin_panel import setup_admin
 from app.core.settings import settings
+from app.db.session import engine
 from app.schemas.users import UserCreate, UserRead, UserUpdate
 
 app = FastAPI(
     title="My Simple Car", openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
+
+setup_admin(app, engine)
 
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
