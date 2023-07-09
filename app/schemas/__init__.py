de2b38890e8092840053a172df60d581178aefed
Kirill_N <sticheg2580@gmail.com>
2023-07-09 17:42:37 +0300

    engine schemas

---
+++ app/schemas/__init__.py
@@ -0,0 +1,1 @@
+from .engines import EngineCreate, EngineRead, EngineUpdate
---
+++ app/schemas/engines.py
@@ -0,0 +1,34 @@
+from enum import Enum
+
+from pydantic import BaseModel
+
+
+class FuelTypeName(str, Enum):
+    diesel = "diesel"
+    gasoline = "gasoline"
+
+
+class EngineRead(BaseModel):
+    id: int
+    model: str
+    fuel_type: FuelTypeName
+    volume: float
+    power: int
+    cars: list = []
+
+    class Config:
+        orm_mode = True
+
+
+class EngineCreate(BaseModel):
+    model: str
+    fuel_type: FuelTypeName
+    volume: float
+    power: int = 100
+
+
+class EngineUpdate(BaseModel):
+    model: str | None
+    fuel_type: FuelTypeName | None
+    volume: float | None
+    power: int | None
