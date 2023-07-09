dafddd7659376beca10b894178e1bc5f351d3988
Kirill_N <sticheg2580@gmail.com>
2023-07-09 19:03:44 +0300

    api router included

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 from fastapi import Depends, FastAPI
-from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.api.api_v1.api import api_router
 from app.auth.config import auth_backend, fastapi_users
 from app.core.admin_panel import setup_admin
 from app.core.settings import settings
-from app.crud import crud_image
 from app.db.session import engine, get_async_session
 from app.schemas.users import UserCreate, UserRead, UserUpdate
 
@@ -38,10 +37,8 @@ app.include_router(
     tags=["Users"],
 )
 
-
-@app.get("/image", tags=["image"])
-async def get_image(
-    id: int, session: AsyncSession = Depends(get_async_session)
-):
-    image = await crud_image.get(session, id)
-    return {"image": image}
+app.include_router(
+    api_router,
+    prefix=settings.API_V1_STR,
+    dependencies=[Depends(get_async_session)],
+)
