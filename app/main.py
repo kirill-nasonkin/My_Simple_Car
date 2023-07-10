8c36292f9239364df5c3842a2f8b8e8f86e4636d
Kirill_N <sticheg2580@gmail.com>
2023-07-10 15:41:57 +0300

    - fastapi users

--- app/main.py
+++ app/main.py
@@ -1,41 +1,15 @@
 from fastapi import Depends, FastAPI
 
 from app.api.api_v1.api import api_router
-from app.auth.config import auth_backend, fastapi_users
 from app.core.admin_panel import setup_admin
 from app.core.settings import settings
 from app.db.session import engine, get_async_session
-from app.schemas.users import UserCreate, UserRead, UserUpdate
 
 app = FastAPI(
     title="My Simple Car", openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
 
 setup_admin(app, engine)
-
-app.include_router(
-    fastapi_users.get_auth_router(auth_backend),
-    prefix="/auth",
-    tags=["Auth"],
-)
-
-app.include_router(
-    fastapi_users.get_register_router(UserRead, UserCreate),
-    prefix="/auth",
-    tags=["Auth"],
-)
-
-app.include_router(
-    fastapi_users.get_reset_password_router(),
-    prefix="/auth",
-    tags=["Auth"],
-)
-
-app.include_router(
-    fastapi_users.get_users_router(UserRead, UserUpdate),
-    prefix="/users",
-    tags=["Users"],
-)
 
 app.include_router(
     api_router,
