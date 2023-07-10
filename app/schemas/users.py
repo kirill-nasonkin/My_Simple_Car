22ed97c8818d5624d406321dc4b27697c9d135b3
Kirill_N <sticheg2580@gmail.com>
2023-07-10 16:10:40 +0300

    new users schemas

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -1,41 +1,35 @@
 from datetime import datetime
-from typing import Optional
 
-from fastapi_users import schemas
-from pydantic import EmailStr
+from pydantic import BaseModel, EmailStr
 
 
-class UserRead(schemas.BaseUser[int]):
-    id: int
-    username: str
-    email: EmailStr
-    registered_at: datetime
-    car_id: Optional[int]
-    driver_license_id: Optional[int]
-    insurance_id: Optional[int]
-    document_id: Optional[int]
-    is_active: Optional[bool] = True
-    is_superuser: Optional[bool] = False
-    is_verified: Optional[bool] = False
+class UserBase(BaseModel):
+    email: EmailStr | None = None
+    full_name: str | None = None
+    registered_at: datetime | None
+    is_active: bool | None = True
+    is_superuser: bool = False
 
 
-class UserCreate(schemas.BaseUserCreate):
-    username: str
+class UserCreate(UserBase):
     email: EmailStr
     password: str
-    is_active: Optional[bool] = True
-    is_superuser: Optional[bool] = False
-    is_verified: Optional[bool] = False
 
 
-class UserUpdate(schemas.BaseUserUpdate):
-    username: Optional[str]
-    email: Optional[EmailStr]
-    password: Optional[str]
-    car_id: Optional[int]
-    driver_license_id: Optional[int]
-    insurance_id: Optional[int]
-    document_id: Optional[int]
-    is_active: Optional[bool]
-    is_superuser: Optional[bool]
-    is_verified: Optional[bool]
+class UserUpdate(UserBase):
+    password: str | None
+
+
+class UserInDBBase(UserBase):
+    id: int | None = None
+
+    class Config:
+        orm_mode = True
+
+
+class UserRead(UserInDBBase):
+    pass
+
+
+class UserInDB(UserInDBBase):
+    hashed_password: str
