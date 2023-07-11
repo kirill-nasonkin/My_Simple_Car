2667b5556b70dbb5bc69e0940a4873aede669359
Kirill_N <sticheg2580@gmail.com>
2023-07-11 15:22:51 +0300

    + login.router

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from .endpoints import engines, users
+from .endpoints import engines, users, login
 
 api_router = APIRouter()
 
+api_router.include_router(login.router, tags=["Login"])
 api_router.include_router(users.router, prefix="/users", tags=["Users"])
 api_router.include_router(engines.router, prefix="/engines", tags=["Engines"])
