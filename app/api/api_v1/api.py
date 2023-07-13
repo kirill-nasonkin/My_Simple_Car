18f9724e242bbd5e1c5ebf28b7c5e5b0714b01b9
Kirill_N <sticheg2580@gmail.com>
2023-07-13 17:32:16 +0300

    login.router, tags

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -4,6 +4,8 @@ from .endpoints import engines, users, login
 
 api_router = APIRouter()
 
-api_router.include_router(login.router, tags=["Login"])
+api_router.include_router(
+    login.router, tags=["Login, logout, password recovery"]
+)
 api_router.include_router(users.router, prefix="/users", tags=["Users"])
 api_router.include_router(engines.router, prefix="/engines", tags=["Engines"])
